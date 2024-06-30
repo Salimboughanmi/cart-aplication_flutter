fbb608cd8ebc628a2bae554ba1c9085132259a6a
salimboughanmi <boughanmi1salim@gmail.com>
2024-06-30 16:31:24 +0100

    add prod to cart

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,7 @@
+import 'package:cart_app_with_provider/model/cart.dart';
 import 'package:cart_app_with_provider/model/product.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -18,11 +20,46 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
         appBar: AppBar(
           title: Text("Home"),
+          centerTitle: true,
+          backgroundColor: Colors.blue,
+          actions: [
+            Row(
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.add_shopping_cart),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(right: 20),
+                  child: Consumer<Cart>(
+                    builder: (BuildContext context, value, child) {
+                      return Text("${value.count}");
+                    },
+                  ),
+                ),
+              ],
+            )
+          ],
         ),
         body: ListView.builder(
             itemCount: products.length,
             itemBuilder: (context, i) {
-              return Card();
+              return Card(
+                child: Consumer<Cart>(
+                  builder: (context, value, child) {
+                    return ListTile(
+                      title: Text("${products[i].name}"),
+                      //subtitle: (Text('${products[i].price}')),
+                      trailing: IconButton(
+                        icon: Icon(Icons.add),
+                        onPressed: () {
+                          value.addProduct(products[i]);
+                        },
+                      ),
+                    );
+                  },
+                ),
+              );
             }));
   }
 }
--- lib/model/cart.dart
+++ lib/model/cart.dart
@@ -1,3 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:cart_app_with_provider/model/product.dart';
 
-class Cart with ChangeNotifier {}
+class Cart with ChangeNotifier {
+  List<Product> _items = [];
+  double _price = 0;
+  int quantity = 0;
+
+  void addProduct(Product product) {
+    _items.add(product);
+    _price += product.price;
+    notifyListeners();
+  }
+
+  int get count {
+    return _items.length;
+  }
+
+  double get price {
+    return _price;
+  }
+}
